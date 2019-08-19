6803e5980fad6d8a59bc4af40cbff96a896054f9
Thomas Cazade <cazade.thomas@gmail.com>
2019-08-19 14:15:02 +0200

    feat(home): fetch home, allBooks and a specific book

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,24 @@
 </template>
 
 <script>
+import { AllBooks, Book, Home } from '../queries';
+
 export default {
   name: 'home',
+
+  apollo: {
+    home: Home,
+    allBooks: AllBooks,
+
+    book() {
+      return {
+        query: Book,
+        variables: {
+          id: '1370262',
+        },
+        fetchPolicy: 'network-only',
+      };
+    },
+  },
 };
 </script>
