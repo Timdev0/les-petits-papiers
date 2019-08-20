92c81aa950a6ab52b0a114e86434d5e1fcb39a0d
Thomas Cazade <cazade.thomas@gmail.com>
2019-08-20 11:10:11 +0200

    feat(home): add carousel + reserve text

--- src/App.vue
+++ src/App.vue
@@ -20,4 +20,10 @@ export default {
 @import '../node_modules/bootswatch/dist/flatly/variables';
 @import '../node_modules/bootstrap/scss/bootstrap';
 @import '../node_modules/bootswatch/dist/flatly/bootswatch';
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  background-color: #ecf0f1;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="home"></div>
+  <div class="home" v-if="!$apollo.loading">
+    <div id="home-carousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li
+          v-for="(carouselItem, index) in home.carousel"
+          :key="'carousel-indicator-' + index"
+          data-target="#home-carousel"
+          :data-slide-to="index"
+          class="active"
+        />
+      </ol>
+
+      <div class="carousel-inner">
+        <div
+          v-for="(carouselItem, index) in home.carousel"
+          :key="'carousel-item-' + index"
+          class="carousel-item"
+          :class="{ 'active': index === 0 }"
+        >
+          <img :src="carouselItem.image.url">
+
+          <div class="carousel-caption d-md-block">
+            <h5>{{ carouselItem.caption }}</h5>
+            <p>{{ carouselItem.subtitle }}</p>
+          </div>
+        </div>
+
+        <a class="carousel-control-prev" href="#home-carousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Précédent</span>
+        </a>
+
+        <a class="carousel-control-next" href="#home-carousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Suivant</span>
+        </a>
+      </div>
+    </div>
+
+    <div class="container reserve-block">
+      <h2>Les réservations</h2>
+
+      <p>{{ home.reserve }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -24,3 +68,40 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.carousel {
+  padding-top: 24px;
+  background-color: #2C3E50 !important;
+}
+
+.carousel-item img {
+  display: block;
+  margin: 0 auto;
+  max-height: 500px;
+}
+
+.carousel-caption h5,
+.carousel-caption p {
+  text-shadow: -1px -1px 3px #222, 1px -1px 3px #222, -1px 1px 3px #222, 1px 1px 3px #222;
+}
+
+.carousel-caption p {
+  margin: 0;
+}
+
+.reserve-block {
+  padding: 16px 24px;
+  margin: 32px auto;
+  border-radius: 4px;
+  background: #fff;
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  p {
+    white-space: pre-wrap;
+  }
+}
+</style>
